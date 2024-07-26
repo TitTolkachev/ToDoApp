a6baf4eef3f6845fd2f45fdb18f764c627c91f39
Tit Tolkachev <tittolkachev@gmail.com>
2024-07-27 00:14:20 +0700

    Элемент списка.

--- feature/todo/src/main/java/com/example/todoapp/feature/todo/todolist/components/TodoListItem.kt
+++ feature/todo/src/main/java/com/example/todoapp/feature/todo/todolist/components/TodoListItem.kt
@@ -1,5 +1,6 @@
 package com.example.todoapp.feature.todo.todolist.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -18,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.CustomAccessibilityAction
 import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.customActions
 import androidx.compose.ui.semantics.selectableGroup
 import androidx.compose.ui.semantics.selected
@@ -34,6 +37,8 @@ import com.example.todoapp.core.designsystem.theme.AppTheme
 import com.example.todoapp.core.model.Importance.HIGH
 import com.example.todoapp.core.model.Importance.LOW
 import com.example.todoapp.core.model.Importance.MEDIUM
+import com.example.todoapp.core.model.TodoItem
+import com.example.todoapp.feature.todo.R
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -41,7 +46,7 @@ import com.example.todoapp.core.designsystem.R as UiR
 
 @Composable
 fun TodoListItem(
-    item: com.example.todoapp.core.model.TodoItem,
+    item: TodoItem,
     onCheckedChange: (Boolean) -> Unit,
     onClick: () -> Unit,
 ) {
@@ -49,20 +54,11 @@ fun TodoListItem(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.surfaceVariant)
             .padding(top = 12.dp, end = 12.dp, bottom = 12.dp, start = 4.dp)
-            .semantics(mergeDescendants = true) {
-                selectableGroup()
-                selected = item.done
-                customActions = listOf(
-                    CustomAccessibilityAction("Отметить выполненным") {
-                        onCheckedChange(!item.done)
-                        true
-                    },
-                    CustomAccessibilityAction("Открыть") {
-                        onClick()
-                        true
-                    }
-                )
-            }
+            .accessibilityDescription(
+                item = item,
+                onClick = onClick,
+                onCheckedChange = onCheckedChange
+            )
     ) {
         Checkbox(
             checked = item.done,
@@ -79,7 +75,7 @@ fun TodoListItem(
                 Image(
                     modifier = Modifier.size(24.dp),
                     painter = painterResource(id = UiR.drawable.ic_low_importance),
-                    contentDescription = "Низкая важность",
+                    contentDescription = null,
                 )
                 Spacer(Modifier.width(4.dp))
             }
@@ -89,7 +85,7 @@ fun TodoListItem(
                 Image(
                     modifier = Modifier.size(24.dp),
                     painter = painterResource(id = UiR.drawable.ic_high_importance),
-                    contentDescription = "Высокая важность",
+                    contentDescription = null,
                 )
                 Spacer(Modifier.width(4.dp))
             }
@@ -165,6 +161,53 @@ private fun Deadline(date: Date) {
     )
 }
 
+@SuppressLint("ComposableNaming")
+@Composable
+private fun Modifier.accessibilityDescription(
+    item: TodoItem,
+    onClick: () -> Unit,
+    onCheckedChange: (Boolean) -> Unit,
+): Modifier {
+    val resources = LocalContext.current.resources
+    val formatter = remember {
+        SimpleDateFormat("d MMMM yyyy", Locale.getDefault())
+    }
+
+    return this.semantics(mergeDescendants = true) {
+        selectableGroup()
+        selected = item.done
+
+        customActions = listOf(
+            CustomAccessibilityAction(
+                label = resources.getString(
+                    if (item.done)
+                        R.string.todo_list_mark_item_not_done
+                    else
+                        R.string.todo_list_mark_item_done
+                )
+            ) {
+                onCheckedChange(!item.done)
+                true
+            },
+            CustomAccessibilityAction(resources.getString(R.string.todo_list_show_item)) {
+                onClick()
+                true
+            }
+        )
+
+        contentDescription = resources.getString(
+            when (item.importance) {
+                LOW -> R.string.todo_list_item_importance_low
+                MEDIUM -> R.string.todo_list_item_importance_medium
+                HIGH -> R.string.todo_list_item_importance_high
+            }
+        ) + item.text + (item.deadline?.let {
+            ". ${resources.getString(R.string.todo_list_item_deadline_description)}" +
+                    formatter.format(it)
+        } ?: "")
+    }
+}
+
 @PreviewLightDark
 @Composable
 private fun Preview() {
@@ -172,7 +215,7 @@ private fun Preview() {
         LazyColumn {
             item {
                 TodoListItem(
-                    item = com.example.todoapp.core.model.TodoItem(
+                    item = TodoItem(
                         id = "1",
                         text = "Buy groceries",
                         importance = MEDIUM,
